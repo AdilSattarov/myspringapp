7bd324a6774db7d6320f5406d656bbc32fcef42d
AdilSattarov <adil20071804@gmail.com>
2021-05-08 00:35:54 +0400

    add project

---
+++ boot/src/main/java/com/spring/boot/rest/NewRestController.java
@@ -0,0 +1,29 @@
+package com.spring.boot.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewRestController {
+	@Value("${myname}")
+	String name;
+	
+	@Value("${myage}")
+	String age;
+@RequestMapping("/")
+public String hello() {
+	return"Hello, time is "+ LocalDateTime.now()+" "+name+" "+age;
+}
+@RequestMapping("/demo")
+public String demo() {
+	return"Hello, test demo";
+}
+@RequestMapping("/toolTest")
+public String test() {
+	return"Hello, test devtool";
+}
+
+}
